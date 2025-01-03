71dcdcbdd9cf9dc6b53e020c3d42cd5fc95333e7
dkl5m <141453881+dkl5m@users.noreply.github.com>
2025-01-03 16:16:27 -0300

    change the loop of LM and NR MultiFunction

---
+++ classes/numerical_methods/NewtonRaphsonMultiFunctionMethod.m
@@ -0,0 +1,43 @@
+% Newton-Raphson Method (NR) to find the
+% root of multiple equations
+clc; clear; close all; % initialize IDE
+
+% initial guess, tolerance, iteration, max iteration
+X = [2 2]'; tol =1e-6; iter = 0; max_iter = 50;
+
+% NEWTON-RAPHSON'S METHOD
+while (err(X) > tol) && (iter < max_iter)
+    s = -inv(J(X))*f(X);
+    X = X + s;
+    iter = iter + 1;
+end
+
+if iter < max_iter
+    disp("Newton-Raphson's method")
+    disp('x1 = '); disp(X(1)); disp('x2 = '); disp(X(2));
+    disp('Error = '); disp(err(X)); disp('Iteration = ');
+    disp(iter);
+else
+    disp("Newton-Raphson's method didn't converge")
+end
+
+% FUNCTIONS
+% error function
+function outerr = err(x)
+out = f(x);
+outerr = out(1)^2+out(2)^2;
+end
+
+% function's vector
+function out = f(x)
+x1 = x(1);
+x2 = x(2);
+out = [x1+2*x2-2; x1^2+4*x2^2-4];
+end
+
+% Jacobian matrix
+function out = J(x)
+x1 = x(1);
+x2 = x(2);
+out = [1, 2; 2*x1, 8*x2];
+end
